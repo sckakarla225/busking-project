73e7d87ae899a392cb37480f15c5d9f686a29b14
Samhith Kakarla <samhith.kakarla@duke.edu>
2024-01-22 22:50:11 -0500

    testing some maps stuff

--- spot-predictor/main.py
+++ spot-predictor/main.py
@@ -4,3 +4,4 @@ import pandas as pd
 st.title("Spot Prediction API")
 st.write("Key Regions: Capital District, Fayetteville Street, Moore Square, Glenwood South, Warehouse District, Social District, Historic Oakwood, Prince Hall/South Park")
 st.write("Target Areas: Hotels, Restaurants/Food Trucks, Music Venues, Museums, Events/Festivals, Nightlife, Stores")
+st.write("Data Collector: https://busking-project.vercel.app/")
