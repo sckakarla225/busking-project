f12b18b9a7a35e2456c0c7b5d4a9bd7353696356
Samhith Kakarla <samhith.kakarla@duke.edu>
2024-03-16 18:54:02 -0400

    quick fix to loading screen

--- interface/components/Loading.tsx
+++ interface/components/Loading.tsx
@@ -11,7 +11,7 @@ const Loading: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <div className="fixed inset-0 z-20 flex justify-center items-center mx-4">
       <div className="flex flex-row">
-        <Image src={'/logos/spotlite-icon.PNG'} alt="logo" width={60} height={60} className="animate-spin" />
+        <Image src={'/logos/spotlite-icon.png'} alt="logo" width={60} height={60} className="animate-spin" />
       </div>
     </div>
   );
